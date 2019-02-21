3615dd542a426dfd43ce066608fa1d2a7f103ddf
ScArYT1 <43856140+ScArYT1@users.noreply.github.com>
2019-02-21 19:25:24 +0200

    Add files via upload

---
+++ Miner/miner.js
@@ -0,0 +1,22 @@
+const MINE_1 = 3;
+const MINE_2 = 7;
+const MINE_3 = 9;
+
+
+var miner = 2;
+
+
+
+
+//let's show zer map :)
+
+
+for (var step = 1; step<=10; step++) {
+  var content =`Opa!;`// `` --> template string
+  document.write(
+    `<div class="grass">
+    ${content}
+    </div>`
+  );
+
+}
